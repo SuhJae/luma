4ea3cd489e660e0b1e8f6e8e966c9254a001c56d
JaeWoong Suh <jwsuh25@kis.ac>
2024-01-05 11:32:12 +0900

    fix: moved event to fetch language first

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    build: {
+        // Use a relative path for outDir
+        outDir: resolve(__dirname, '../backend/app/static'),
+        // This will empty the outDir folder before building
+        emptyOutDir: true,
+    }
 })
